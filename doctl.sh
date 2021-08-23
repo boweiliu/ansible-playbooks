2dd81a73a6fcee14e748d32882326237705bfb78
Bowei Liu <liubowei@gmail.com>
2021-08-23 09:58:49 -0700

    add alternative size

--- doctl.sh
+++ doctl.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 doctl compute droplet create --image ubuntu-20-04-x64 --size s-2vcpu-4gb-intel --region sfo3 --enable-private-networking --ssh-keys 31067977,31059354 desktop
+
+#doctl compute droplet create --image ubuntu-20-04-x64 --size s-4vcpu-8gb-intel --region sfo3 --enable-private-networking --ssh-keys 31067977,31059354 desktop
