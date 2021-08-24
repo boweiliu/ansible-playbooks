3cc0d15e7ec37082bd8847eb79377ccb72b80357
Bowei Liu <liubowei@gmail.com>
2021-08-24 09:46:01 -0700

    add up/down for doctl script

--- doctl.sh
+++ doctl.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-doctl compute droplet create --image ubuntu-20-04-x64 --size s-2vcpu-4gb-intel --region sfo3 --enable-private-networking --ssh-keys 31067977,31059354,31100429 --wait desktop
+set -x
 
-#doctl compute droplet create --image ubuntu-20-04-x64 --size s-4vcpu-8gb-intel --region sfo3 --enable-private-networking --ssh-keys 31067977,31059354,31100429 --wait desktop
+if [[ "$1" = down ]] ;
+then
+	doctl compute droplet delete --force desktop
+else
+	doctl compute droplet create --image ubuntu-20-04-x64 --size s-2vcpu-4gb-intel --region sfo3 --enable-private-networking --ssh-keys 31067977,31059354,31100429 --wait desktop
+	#doctl compute droplet create --image ubuntu-20-04-x64 --size s-4vcpu-8gb-intel --region sfo3 --enable-private-networking --ssh-keys 31067977,31059354,31100429 --wait desktop
+fi
