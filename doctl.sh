02755f02fedbb96f11c24ea42e38fd96af0a4858
Bowei Liu <liubowei@gmail.com>
2022-08-14 22:09:26 +0000

    fixup bootstrap

--- doctl.sh
+++ doctl.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir -p /etc/ansible
+touch /etc/ansible/hosts
+
 set -x
 
 if [[ "$1" = down ]] ;
@@ -9,7 +12,9 @@ else
 	#doctl compute droplet create --image ubuntu-20-04-x64 --size s-2vcpu-4gb-intel --region sfo3 --enable-private-networking --ssh-keys 31067977,31059354,31100429 --wait desktop
 	doctl compute droplet create --image ubuntu-22-04-x64 --size s-2vcpu-4gb-intel --region sfo3 --enable-private-networking \
 		--ssh-keys 35943893,35561772,31067977,31059354,31100429 --wait desktop"$TEST" \
-		-o json | tee -a /tmp/doctl.out
+		-o json | tee -a /tmp/doctl.out \
+		jq -r ' .[0].networks.v4 | .[] | select(.type == "public") | .ip_address ' >> /etc/ansible/hosts
+# jout[0].networks.v4 | filter(.type = public) | .ip_address  |
 	#doctl compute droplet create --image ubuntu-20-04-x64 --size s-4vcpu-8gb-intel --region sfo3 --enable-private-networking --ssh-keys 31067977,31059354,31100429 --wait desktop
 fi
 
@@ -21,4 +26,3 @@ fi
 # 31100429    bowei@work-laptop.local bowei-hatch-github    35:1e:2a:fd:94:19:d9:26:be:9a:17:e8:c2:5d:c2:21
 # 31067977    work-laptop                                   0a:b8:d0:28:5d:9f:5f:0e:80:15:3c:9d:73:d0:3d:3e
 # 31059354    bowei@Boweis-MacBook-Pro.local                d7:cd:fa:cb:af:15:1f:3d:ce:fe:f9:14:da:59:63:7b
-jout[0].networks.v4 | filter(.type = public) | .ip_address 
