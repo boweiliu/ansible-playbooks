fa8cc81ddc13941e70bfb6ff7e2c0ee23c7a5d1d
bowei liu <liubowei@gmail.com>
2022-08-15 18:15:29 +0000

    improve

--- ansible.sh
+++ ansible.sh
@@ -2,6 +2,11 @@
 
 cd setup_ubuntu_users
 
+export ANSIBLE_HOST_KEY_CHECKING=false
+
 ansible-playbook playbook.yml || 
 ansible-playbook -u bowei playbook.yml
+
+ansible-playbook post.yml || 
+ansible-playbook -u bowei post.yml
 
