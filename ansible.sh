4c469bc2f237b1937120e27585ff087b01367c7f
Bowei Liu <liubowei@gmail.com>
2022-08-17 04:27:10 +0000

    try wait first

--- ansible.sh
+++ ansible.sh
@@ -4,6 +4,10 @@ cd setup_ubuntu_users
 
 export ANSIBLE_HOST_KEY_CHECKING=false
 
+ansible-playbook wait.yml || 
+ansible-playbook -u bowei wait.yml
+
+
 ansible-playbook playbook.yml || 
 ansible-playbook -u bowei playbook.yml
 
